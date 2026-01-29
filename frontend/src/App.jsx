bb984aa1a9cad9be88f0537b2d07a0868213f702
Ahmad Ayaz <129485327+ahmadayaz2022@users.noreply.github.com>
2026-01-29 16:24:46 +0500

    Update routing structure in App.jsx
    Refactor routes and improve comments for clarity.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-import EmployeeDashboard from "./pages/Employee/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 // Admin Pages
@@ -9,14 +8,14 @@ import AdminLayout from "./pages/Admin/AdminLayout";
 import AdminDashboard from "./pages/Admin/Dashboard";
 import Stores from "./pages/Admin/Stores";
 import DashboardStores from "./pages/Admin/DashboardStores";
-
 import StorePage from "./pages/Admin/StorePage";
-
 import Employees from "./pages/Admin/Employees";
 import StoreDetails from "./pages/Admin/StoreDetails";
+//
 
 // Employee Pages
 import EmployeeLayout from "./pages/Employee/EmployeeLayout";
+import EmployeeDashboard from "./pages/Employee/Dashboard";
 
 function App() {
   return (
@@ -26,6 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
 
         {/* Admin Routes */}
+
         <Route
           path="/admin/*"
           element={
@@ -34,27 +34,24 @@ function App() {
             </ProtectedRoute>
           }
         >
-          {/* Dashboard */}
+          {/* Admin dashboard */}
           <Route path="dashboard" element={<AdminDashboard />} />
-          {/* <Route path="dashboard/stores" element={<StoreDetails />} /> */}
-
           <Route path="dashboard/stores" element={<DashboardStores />} />
+          <Route path="seeStores" element={<DashboardStores />} />
+
           <Route path="store/:storeId" element={<StorePage />} />
 
-          {/* Stores */}
+          {/* ADMIN Stores */}
           <Route path="stores" element={<Stores />} />
           <Route path="stores/:storeId" element={<StoreDetails />} />
 
-          {/* Employees */}
+          {/* Admin Employees */}
           <Route path="employees" element={<Employees />} />
           <Route path="dashboard/employees" element={<Employees />} />
-
-
+          <Route path="seeEmployees" element={<Employees />} />
         </Route>
 
-
-
-        {/* Employee Routes */}
+        {/* Employee Routes ProtectedRoute.jsx*/}
         <Route
           path="/employee/*"
           element={
@@ -72,5 +69,5 @@ function App() {
     </Router>
   );
 }
-
+//Pack my box with five dozen liquor jugs.
 export default App;
