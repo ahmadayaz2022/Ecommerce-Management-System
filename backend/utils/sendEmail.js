cb76ed4ecfdaff319348057f3bf3ac047d313e0c
Ahmad Ayaz <129485327+ahmadayaz2022@users.noreply.github.com>
2026-02-16 15:16:26 +0500

    Add sendEmail utility using nodemailer

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (to, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.verify(); // 🔥 important
+    console.log("SMTP Connected");
+
+    await transporter.sendMail({
+      from: `"superadmin Admin" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+    throw error;
+  }
+};
+
+module.exports = sendEmail;
