939af32974bd2121c74af50468915b416bb38652
Ahmad Ayaz <129485327+ahmadayaz2022@users.noreply.github.com>
2026-02-17 12:57:09 +0500

    added more details to the email which is sended to the employee during acc creation
    Updated email content and added additional employee details in the response.

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -1,8 +1,5 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-// SuperAdmin creates employee
-
-
 const sendEmail = require("../utils/sendEmail");
 
 exports.createEmployee = async (req, res) => {
@@ -27,7 +24,7 @@ exports.createEmployee = async (req, res) => {
     if (userExists) {
       return res.status(400).json({ message: "Employee already exists" });
     }
-
+    
     // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -51,19 +48,29 @@ exports.createEmployee = async (req, res) => {
 
     // EMAIL CONTENT
     const emailContent = `
-      <h2>Welcome to Our Company</h2>
+      <h2>Welcome to Our Ecomerce STORE</h2>
       <p>Hello <b>${name}</b>,</p>
-      <p>Your employee account has been created.</p>
+      <p>Your employee account has been created, below is your Account detials</p>
       <p><b>Email:</b> ${email}</p>
       <p><b>Password:</b> ${password}</p>
-      <p>Please change your password after first login.</p>
+
+      <p>FatherName: <b>${fathername}</b>,</p>
+      <p><b>PhoneNumber: </b> ${PhoneNumber} </p>
+      <p><b>StoreID: </b> ${storeId} </p>
+      <p><b>COUNTRY: </b> ${city} </p>
+      <p><b>CNIC: </b> ${cnic} </p>
+      <p><b>postal Address: </b> ${PostalAddress} </p>
+      <p><b>nationality: </b> ${nationality} </p>
+      <p><b>dateOfBirth: </b> ${dateOfBirth} </p>
+      <p><b>Religion: </b> ${Religion} </p>
+
+      
+      <p>Please Save your password after first login.</p>
       <br/>
       <p>Regards,<br/>Admin Team</p>
     `;
-
     // SEND EMAIL
     await sendEmail(email, "Employee Account Created", emailContent);
-
     res.status(201).json({
       message: "Employee created and email sent",
       employee: {
@@ -71,15 +78,22 @@ exports.createEmployee = async (req, res) => {
         name: employee.name,
         email: employee.email,
         store: employee.store,
+        fathername: employee.fathername,
+        dateOfBirth: employee.dateOfBirth,
+        profilePicture: employee.profilePicture,
+        nationality: employee.nationality,
+        city: employee.city,
+        cnic: employee.cnic,
+        Religion: employee.Religion,
+        PostalAddress: employee.PostalAddress,
+        PhoneNumber: employee.PhoneNumber,
       },
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
   }
 };
-
 
  
 exports.editEmployee = async (req, res) => {
